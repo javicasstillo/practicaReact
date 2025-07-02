805818e8f1b01f42f3ce76b8476deafed93d1947
javier <javiercastillo.tuc@gmail.com>
2025-07-02 12:23:17 -0300

    se actualiza ejercicio 1

--- src/components/PrimerEjercicio.jsx
+++ src/components/PrimerEjercicio.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PrimerEjercicio(){
 
@@ -12,9 +12,13 @@ function PrimerEjercicio(){
     setNumeroInicial(numeroInicial-1)
   }
 
+  useEffect(()=>{
+    document.title = numeroInicial
+  })
+
   return <div className="text-center bg-body-tertiary py-3">
     <h1>Primer ejercicio</h1>
-    <p>Realizar un contador: Al hacer click en boton "+" que el numero incremente y al hacer click en el boton "-" que el numero decremente"</p>
+    <p>Realizar un contador: Al hacer click en boton "+" que el numero incremente y al hacer click en el boton "-" que el numero decremente". Usar useEffect para que el titulo tambien cambie con el numero del contador. </p>
     <div className="d-flex justify-content-center gap-3">
       <button className="btn btn-lg btn-danger" onClick={restar}>-</button>
       <h3 className="mt-2">{numeroInicial}</h3>
